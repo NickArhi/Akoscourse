549b723d1d31533239cf5843aa60dc2a0d7e783a
NickArhi <61687202+NickArhi@users.noreply.github.com>
2022-04-15 15:22:55 +0300

    Add files via upload

--- ca/lab09/main.c
+++ ca/lab09/main.c
@@ -1,4 +1,6 @@
+//task2
 #include <stdio.h>
+#include <stdlib.h>
 
 void swap(int *x, int *y){
     int z = *x;
@@ -7,10 +9,18 @@ void swap(int *x, int *y){
 }
 
 int main() {
-    int x, y;
-    scanf("%d %d", &x, &y);
-    swap(&x, &y);
-    printf("%d %d", x, y);
+    int N;
+    scanf("%d", &N);
+    int* p1 = malloc(N*sizeof(int));
+    for (int i = 0; i < N; ++i) {
+        scanf("%d", &p1[i]);
+    }
+    for (int j = 0; j < N/2; ++j){
+        swap(&p1[j], &p1[N-j-1]);
+    }
+    for (int k = 0; k < N; ++k){
+        printf("%d\n", p1[k]);
+    }
+    free(p1);
     return 0;
 }
-
